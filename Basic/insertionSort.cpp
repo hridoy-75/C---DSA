473bacfd022e0a425d03505207475114bd06614c
hridoy-75 <hcsarker2002@gmail.com>
2024-11-02 21:36:52 +0600

    hi

--- Basic/insertionSort.cpp
+++ Basic/insertionSort.cpp
@@ -4,8 +4,25 @@ using namespace std;
 int main()
 {
    int arr[] = {7,8,3,1,2};
+   int n = sizeof(arr) / sizeof(arr[0]);
 
    //insertionsort
+   for(int i=0; i<n; i++){
+    int current = arr[i];
+    int j= i-1;
+    while(j>=0 && current< arr[j]) {
+        arr[j+1]= arr[j];
+        j--;
+        }
+        arr[j+1] = current;
+     }
 
-   for(int i=0; i<arr.length)
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
